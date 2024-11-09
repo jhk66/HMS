9218d7d055e2b8c7ab2657f701e5f9b5ab1655fb
jhk66 <choun999@naver.com>
2024-11-09 21:04:17 +0900

    aaa

--- src/main/java/deu/hms/hotelrestaurant/TestMain.java
+++ src/main/java/deu/hms/hotelrestaurant/TestMain.java
@@ -15,7 +15,7 @@ public class TestMain {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("HelloWorld");
+        System.out.println("HelloJAVA");
         System.out.println("HaHa");
     }
     
