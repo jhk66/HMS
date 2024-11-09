d60a21957acb5918ee9aa7cbf9c5c0253b2135b2
MinTTEO <leeminseok5209@naver.com>
2024-11-09 21:04:36 +0900

    Update TestMain.java

--- src/main/java/deu/hms/hotelrestaurant/TestMain.java
+++ src/main/java/deu/hms/hotelrestaurant/TestMain.java
@@ -15,7 +15,6 @@ public class TestMain {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("HelloWorld");
         System.out.println("HaHa");
     }
     
