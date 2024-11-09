a39009a7932d91d91498760d9e08238b33f19137
jhk66 <choun999@naver.com>
2024-11-09 21:01:57 +0900

    UpTest

--- src/main/java/deu/hms/hotelrestaurant/TestMain.java
+++ src/main/java/deu/hms/hotelrestaurant/TestMain.java
@@ -16,6 +16,7 @@ public class TestMain {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("HelloWorld");
+        System.out.println("HaHa");
     }
     
 }
